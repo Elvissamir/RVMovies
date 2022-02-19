ece0ca9c223c02f825e58e798fa22b636b2b7d5e
Elvis <elvissamir18@gmail.com>
2022-02-19 14:02:42 -0400

    Movies component shows the number of movies in the list, and allows deleting movies.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 function Movies() {
-  const movies = [
+  const moviesArray = [
     { id: 0, title: "Movie 1", genre: "Action", numberInStock: 6, rate: 2.5 },
     { id: 1, title: "Movie 2", genre: "Romance", numberInStock: 4, rate: 2.5 },
     { id: 2, title: "Movie 3", genre: "Adventure", numberInStock: 5, rate: 2.5,},
@@ -10,12 +10,17 @@ function Movies() {
     { id: 4, title: "Movie 5", genre: "Action", numberInStock: 100, rate: 3 },
   ];
 
-  const [moviesCount, setMoviesCount] = useState(movies.length);
+  const [movies, setMovies] = useState(moviesArray);
+
+  const handleDelete = (movie) => {
+    const m = movies.filter((item) => movie.id != item.id)
+    setMovies(m) 
+  }
 
   const renderMoviesCount = () => {
     const noMoviesMessage = "There are no movies to show";
-    const countMessage = `Showing ${moviesCount} movies in the database`;
-    const content = moviesCount === 0 ? noMoviesMessage : countMessage;
+    const countMessage = `Showing ${movies.length} movies in the database`;
+    const content = movies.length === 0 ? noMoviesMessage : countMessage;
 
     return <p>{content}</p>;
   };
@@ -24,7 +29,7 @@ function Movies() {
     const headers = ["Title", "Genre", "Stock", "Rate", " "];
 
     return (
-      <table className="table-auto w-full">
+      <table className="table-fixed w-full">
         <thead>
           <tr className="">
             { headers.map(header => (
@@ -39,6 +44,11 @@ function Movies() {
                     <td>{ movie.genre }</td>
                     <td>{ movie.numberInStock }</td>
                     <td>{ movie.rate }</td>
+                    <td className="text-center">
+                      <button onClick={ () => handleDelete(movie) } className="bg-red-700 px-2 py-1 font-black text-white">
+                        Delete
+                      </button>
+                    </td>
                 </tr>
             )) }
         </tbody>
@@ -46,9 +56,14 @@ function Movies() {
     );
   };
 
+  if (movies.length === 0)
+    return  (<div className="mt-8">
+              <div>{ renderMoviesCount() }</div>
+            </div>)
+
   return (
     <div className="mt-8">
-      <div>{renderMoviesCount()}</div>
+      <div>{ renderMoviesCount() }</div>
       <div className="mt-4">{renderMoviesList()}</div>
     </div>
   );
