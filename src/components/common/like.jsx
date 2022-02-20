a479511d9fc0d833b752bd2ee6714659de6eb79d
Elvis <elvissamir18@gmail.com>
2022-02-20 10:55:33 -0400

    Added proptypes ckecking to like component.

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,15 +1,21 @@
+import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-function Likes (props) {
+function Likes ({ liked, onClick }) {
     const likedClass = 'text-red-800'
     const defaultClass = 'text-gray-800'
     let activeClass = defaultClass
 
-    activeClass = (props.liked)? likedClass:defaultClass
+    activeClass = (liked)? likedClass:defaultClass
     
-    return <button onClick={ props.onClick }>
+    return <button onClick={ onClick }>
         <FontAwesomeIcon className={ activeClass } icon={'heart'} />
     </button>
+}
+
+Likes.propTypes = {
+    liked: PropTypes.bool.isRequired,
+    onClick: PropTypes.func.isRequired
 }
 
 export default Likes
