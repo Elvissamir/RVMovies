932b4bffaf89fbdbe97226e17a3b56c00867555c
Elvis <elvissamir18@gmail.com>
2022-02-28 05:53:30 -0400

    Modified filterList.js logic. Now it filters the movies by searching in the genres array property of each movie.

--- src/utils/filterList.js
+++ src/utils/filterList.js
@@ -2,5 +2,6 @@
 export function filterList (list, option) {
     if (option === 'All')
         return list
-    return list.filter(item => item.genre === option)
+
+    return list.filter(item => item.genres.some(value => value.name === option))
 }
